fe6e38c155ecde60379ac9c51687cecfe9acc568
wooldridge12 <wooldridgemikey@gmail.com>
2021-11-15 17:24:42 -0500

    Auth amd navbar built out

--- src/components/database/sharkInfo.js
+++ src/components/database/sharkInfo.js
@@ -1,3 +1,4 @@
+//This will be used for survived and eaten page.
 import React, { useState, createContext } from "react"
 
 export const SharkInfoContext = createContext()
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("dive_token")
+                                props.history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
